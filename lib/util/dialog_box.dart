e188f60d7095f135e82b75d983b92471e5a40ae7
BW404 <taj.darsana@gmail.com>
2025-02-16 22:59:37 +0600

    Update: 2025-02-16 22:59:37

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -1,46 +1,5 @@
 import 'package:flutter/material.dart';
 
-class ToDoTile extends StatelessWidget {
-  final String taskName;
-  final bool taskCompleted;
-  Function(bool?)? onChanged;
-
-  ToDoTile({
-    Key? key,
-    required this.taskName,
-    required this.taskCompleted,
-    required this.onChanged,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0, bottom:0),
-      child: Container(
-        padding: const EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          color: taskCompleted ? Colors.deepOrange[100] : Colors.deepOrangeAccent[200],
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          children: [
-            // Checkbox
-            Checkbox(value: taskCompleted,
-             onChanged: onChanged,
-             activeColor: Colors.red),
-            // Task name
-            Text(
-              taskName,
-              style: TextStyle(
-                color: taskCompleted ? Colors.grey : Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+class DialogBox extends StatelessWidget {
+  const DialogBox(super.key());
 }
