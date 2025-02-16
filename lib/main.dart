1c35347c7ca87d6b5db8c9dba8bfac988d309e84
BW404 <taj.darsana@gmail.com>
2025-02-16 18:33:03 +0600

    Update: 2025-02-16 18:33:03

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class dinotodo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    debugInvertOversizedImages = false;
     return MaterialApp(
       title: 'Dino Todo',
       theme: ThemeData(
