9d85c1988ba681d87ec6f06dd98eb770666fa7ce
BW404 <taj.darsana@gmail.com>
2025-02-16 18:16:35 +0600

    Update: 2025-02-16 18:16:35

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dinotodo/pages/home_page.dart';
+import 'package:dinotodo/lib/pages/home_page.dart';
 
 void main() {
   runApp(const dinotodo());
