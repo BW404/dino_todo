85392981a10e9226fc8d234e18a864e0e74d93aa
BW404 <taj.darsana@gmail.com>
2025-02-16 18:05:15 +0600

    Update: 2025-02-16 18:05:15

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const dinotodo());
+}
+
+class dinotodo extends StatelessWidget {
+  const dinotodo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dino Todo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
 }
