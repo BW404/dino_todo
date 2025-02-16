45763f6ee0b6628823b1881cc0c203c556fa1e4c
BW404 <taj.darsana@gmail.com>
2025-02-16 18:14:54 +0600

    Update: 2025-02-16 18:14:54

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold();
   }
-}
