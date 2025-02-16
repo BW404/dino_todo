4356d28ef34d8c20fcb5762a287f515abff101f0
BW404 <taj.darsana@gmail.com>
2025-02-16 23:45:13 +0600

    Update: 2025-02-16 23:45:13

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -36,9 +36,9 @@ class DialogBox extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 MyButton(text: "Save", onPressed: () {}),
-                Padding(padding:const EdgeInsets.only(left: 20, top: 20),),
 
                 MyButton(text: "Cancel", onPressed: () {Navigator.of(context).pop();},),
+                Padding(padding:const EdgeInsets.only(left: 20, top: 20),),
               ],
             )
             ],
