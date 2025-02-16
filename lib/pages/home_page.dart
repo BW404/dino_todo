916339385d7cc64cf2294401170421e71a2b0765
BW404 <taj.darsana@gmail.com>
2025-02-16 22:21:51 +0600

    Update: 2025-02-16 22:21:51

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List toDoList = [
     ["Hello world", false],
-    ["Learn Flutter", true],
+    ["Learn Flutter", false],
     ["Take a nap", false],
     ["Play games", false],
     ["Eat dinner", false],
