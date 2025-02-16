36c2b5e22f972228b15b79c7b39d1649bc747a85
BW404 <taj.darsana@gmail.com>
2025-02-16 18:15:31 +0600

    Update: 2025-02-16 18:15:31

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,10 @@ class HomePage extends StatelessWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
   }
+}
