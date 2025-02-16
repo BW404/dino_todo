71b1a3e342edf630127b15e9b588eab7574636ee
BW404 <taj.darsana@gmail.com>
2025-02-16 23:05:33 +0600

    Update: 2025-02-16 23:05:33

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -6,21 +6,7 @@ class DialogBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text('Create a new task'),
-      content: Column(
-        children: [
-          TextField(
-            decoration: const InputDecoration(
-              hintText: 'Task name',
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Create task'),
-          ),
-        ],
-      ),
+      backgroundColor: Colors.deepOrangeAccent[100],
     );
-  }
+}
 }
